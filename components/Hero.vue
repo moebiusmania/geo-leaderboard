b7e4bf5be06e70cf1332684d1ad3dab4fb4cc65a
moebiusmania <moebiusmania@gmail.com>
2025-01-24 18:08:30 +0100

    feat: hero shows season timeframe

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,40 +1,71 @@
 <script setup lang="ts">
-const props = defineProps<{
+import type { Season } from "~/server/api/seasons";
+
+const { season } = defineProps<{
   season: Season;
 }>();
+
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("it-IT", {
+    day: "2-digit",
+    month: "long",
+  });
+};
 </script>
 
 <template>
-  <hgroup>
-    <h1>Geografia - Season Pass</h1>
-    <h2>Season {{ season.number }} - "{{ season.title }}"</h2>
-  </hgroup>
+  <header>
+    <hgroup>
+      <h1>Geografia - Season Pass</h1>
+      <h2>Season {{ season.number }} - "{{ season.title }}"</h2>
+    </hgroup>
+    <!-- display the current season start date and end date formatted as dd/mm/yyyy -->
+    <p>
+      <span>{{ formatDate(season.start) }}</span>
+      <span>-</span>
+      <span>{{ formatDate(season.end) }}</span>
+    </p>
+  </header>
 </template>
 
 <style scoped>
-hgroup {
+header {
   font-family: var(--title-font);
-  font-weight: 400;
-  font-style: normal;
-
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
   text-align: center;
+  margin-bottom: 2.5rem;
 
-  & h1 {
-    font-size: 4.2rem;
-    margin: 0.5rem 0 0;
-    color: var(--primary);
-    text-shadow: 2px 2px 0 var(--text);
-    letter-spacing: 0.1rem;
+  & hgroup {
+    font-weight: 400;
+    font-style: normal;
+
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    & h1 {
+      font-size: 4.2rem;
+      margin: 0.5rem 0 0;
+      color: var(--primary);
+      text-shadow: 2px 2px 0 var(--text);
+      letter-spacing: 0.1rem;
+    }
+
+    & h2 {
+      font-size: 3rem;
+      margin: 0;
+      /* color: var(--secondary); */
+      /* text-shadow: 0 0 0.2rem var(--primary); */
+    }
   }
 
-  & h2 {
-    font-size: 3rem;
-    margin: 0 0 2rem;
-    /* color: var(--secondary); */
-    /* text-shadow: 0 0 0.2rem var(--primary); */
+  & p {
+    margin-top: 0.2rem;
+    font-style: italic;
+    text-transform: capitalize;
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
+    font-weight: bold;
   }
 }
 </style>
